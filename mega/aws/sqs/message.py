afc9d6e17827db55baffa0b7b8bf7982c126855f
Felipe Dornelas <m@felipedornelas.com>
2020-05-19 04:17:39 -0300

    Uses a Marshmallow schema to deserialize SQS messages

--- mega/aws/sqs/message.py
+++ mega/aws/sqs/message.py
@@ -1,26 +1,22 @@
 from typing import Optional
 
 from mega.aws.message import Message, PayloadType, Payload, MessageType
-from mega.aws.payload import deserialize_payload
-# TODO: test
-from mega.aws.sns.schema import matches_sns_message, deserialize_sns_message
 
 
 class SqsMessage(Message):
-    def __init__(self, message_id: str, receipt_handle: str, body: str):
+    def __init__(
+            self,
+            message_id: str,
+            receipt_handle: str,
+            payload: Payload,
+            payload_type: PayloadType,
+            embedded_message: Optional[Message] = None
+    ):
         self._message_id = message_id
         self._receipt_handle = receipt_handle
-
-        payload, payload_type = deserialize_payload(body)
-        if payload_type == PayloadType.DATA and matches_sns_message(payload):
-            sns = deserialize_sns_message(payload)
-            self._payload = sns.payload
-            self._payload_type = sns.payload_type
-            self._embedded_message = sns
-        else:
-            self._payload = payload
-            self._payload_type = payload_type
-            self._embedded_message = None
+        self._payload = payload
+        self._payload_type = payload_type
+        self._embedded_message = embedded_message
 
     @property
     def message_id(self) -> str:
@@ -45,21 +41,3 @@ class SqsMessage(Message):
     @property
     def receipt_handle(self) -> str:
         return self._receipt_handle
-
-    # TODO: test
-    @classmethod
-    def matches(cls, data: dict) -> bool:
-        return (
-                'MessageId' in data and
-                'ReceiptHandle' in data and
-                'Body' in data
-        )
-
-    # TODO: test
-    @classmethod
-    def deserialize(cls, data: dict) -> 'SqsMessage':
-        return SqsMessage(
-            message_id=data['MessageId'],
-            receipt_handle=data['ReceiptHandle'],
-            body=data['Body']
-        )
---
+++ mega/aws/sqs/schema.py
@@ -0,0 +1,53 @@
+from marshmallow import Schema, EXCLUDE, fields, post_load
+
+from mega.aws.payload import deserialize_payload, PayloadType
+from mega.aws.sns.schema import matches_sns_message, deserialize_sns_message
+from mega.aws.sqs.message import SqsMessage
+
+
+class SqsSchemaError(Exception):
+    pass
+
+
+class SqsMessageSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
+
+    message_id = fields.String(data_key='MessageId', required=True, allow_none=False)
+    topic_arn = fields.String(data_key='ReceiptHandle', required=True, allow_none=False)
+    body = fields.String(data_key='Body', required=True, allow_none=False)
+
+    @post_load
+    def build_object(self, data, **kwargs):
+        payload, payload_type = deserialize_payload(data['body'])
+        if payload_type == PayloadType.DATA and matches_sns_message(payload):
+            return self.__build_sqs_message_with_embedded_sns_message(data, payload)
+        return self.__build_sqs_message_with_payload(data, payload, payload_type)
+
+    @staticmethod
+    def __build_sqs_message_with_embedded_sns_message(data, payload):
+        sns_message = deserialize_sns_message(payload)
+        return SqsMessage(
+            message_id=data['message_id'],
+            receipt_handle=data['receipt_handle'],
+            payload=sns_message.payload,
+            payload_type=sns_message.payload_type,
+            embedded_message=sns_message
+        )
+
+    @staticmethod
+    def __build_sqs_message_with_payload(data, payload, payload_type):
+        return SqsMessage(
+            message_id=data['message_id'],
+            receipt_handle=data['receipt_handle'],
+            payload=payload,
+            payload_type=payload_type,
+            embedded_message=None
+        )
+
+    def handle_error(self, exc, data, **kwargs):
+        raise SqsSchemaError("Could not deserialize SQS message: {0}".format(exc))
+
+
+def deserialize_sqs_message(data: dict) -> SqsMessage:
+    return SqsMessageSchema().load(data)
