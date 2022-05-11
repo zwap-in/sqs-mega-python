a737f26a8d426a5c73ba9b37eab361bf0cdccc94
taa <adornettoluigi98@gmail.com>
2022-05-11 11:50:19 +0200

    version 0.0.2: fixed send message params

--- sqs_mega_python_zwap/aws/sqs/publish/api.py
+++ sqs_mega_python_zwap/aws/sqs/publish/api.py
@@ -10,17 +10,27 @@ class SqsPublisher(BaseSqsApi, Publisher):
 
     def publish(
             self, payload: MessagePayload,
-            binary_encoding=False, queue_url: Optional[str] = None, **_kwargs
+            binary_encoding=False,
+            delay_seconds: Optional[int] = None,
+            queue_url: Optional[str] = None, **_kwargs
     ) -> str:
         serialized = serialize_payload(payload, binary_encoding=binary_encoding)
-        return self.publish_raw_message(serialized, queue_url=queue_url)
+        return self.publish_raw_message(serialized,
+                                        delay_seconds=delay_seconds,
+                                        queue_url=queue_url)
 
-    def publish_raw_message(self, body: str, queue_url: Optional[str] = None, **_kwargs) -> str:
+    def publish_raw_message(self, body: str,
+                            delay_seconds: Optional[int] = None,
+                            queue_url: Optional[str] = None,
+                            message_attributes: Optional[dict] = None,
+                            **_kwargs) -> str:
         queue_url = self._get_queue_url(queue_url)
 
         response = self._client.send_message(
             QueueUrl=queue_url,
             MessageBody=body,
+            DelaySeconds=delay_seconds,
+            MessageAttributes=message_attributes
         )
 
         message_id = response.get('MessageId')
